a384987295dfe52ee7f51acdba7e348f9434f943
zab465 <valentina.laugeri@gmail.com>
2023-10-30 18:05:48 +0000

    development to argument in the function

--- Function.R
+++ Function.R
@@ -4,12 +4,18 @@
 
 data("trees")
 
-
-tibble_units <- function(data_set){
+tibble_units <- function(data_set, action){
   if (!is.data.frame(data_set)){
     stop("Input is not a data frame.")
-  }else{
-    tibble::as_tibble(data_set)
+    }else if (action == "mm"){
+      mm <- data_set[, 1] * 25.4
+      return(mm)
+    }else if (action == "metre"){
+      mtr <- data_set[, 1] * 0.0254
+      return(mtr)
+    }else{
+      tibble::as_tibble(data_set)
+    }
   }
-}
+
 
